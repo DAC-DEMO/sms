b7ea3def4d3c9d22fed53c93d8ce5d065bdefb74
santosh <santosh.ece06@gmail.com>
2017-07-16 11:50:30 +0530

    initial project setup.

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+
+var path = require('path')
+var url = require('url')
+
+var win;
+
+function createWindow() {
+    //console.log(__dirname);
+
+    // Create the browser window.
+    win = new BrowserWindow({ width: 800, height: 600 })
+
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'src/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+
+    // Emitted when the window is closed.
+    win.on('closed', function() {
+        win = null
+    })
+}
+
+app.on('ready', createWindow)
