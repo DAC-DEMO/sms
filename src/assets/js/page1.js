d7f9c30e724fc93f716e1675808607538475f49a
santosh <santosh.ece06@gmail.com>
2017-07-16 14:20:17 +0530

    basic data bind.

--- src/assets/js/page1.js
+++ src/assets/js/page1.js
@@ -1,7 +1,8 @@
 var app = angular.module("app");
 
-app.controller("page1", function(){
+app.controller("page1", function($scope) {
 
     // SOURCE OF DATA FOR PAGE1
+    $scope.page1 = { "title": "Page 1" };
 
 });
