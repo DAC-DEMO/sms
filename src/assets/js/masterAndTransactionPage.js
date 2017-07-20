96c77d7a232a87c605c0a14b6c13cc8275211bd0
laxman3194 <laxman3194@gmail.com>
2017-07-21 04:35:57 +0530

    Some footer style is changed and member bill receipt page is linked

--- src/assets/js/masterAndTransactionPage.js
+++ src/assets/js/masterAndTransactionPage.js
@@ -1,9 +1,14 @@
 var app = angular.module("app");
 
-app.controller("masterAndTransactionPage", function($scope) {
+app.controller("masterAndTransactionPage", function($scope,$location) {
 
     // SOURCE OF DATA FOR PAGE1
     $scope.masterAndTransactionPage = { "title" : "Master and Transaction Page" };
 
-    
+    $scope.memberBillReceipt = function(){
+
+        
+        $location.path("/memberReceipt");
+
+    };    
 });
