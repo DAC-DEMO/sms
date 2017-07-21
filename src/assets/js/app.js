eb8acb92d38245507b917b92d6b81185033683cf
KundanPatil <kkpatil19@gmail.com>
2017-07-21 15:58:03 +0530

    SocVoucher, updateMember, addMember pages updated..!!

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -19,20 +19,35 @@ app.config(function($routeProvider) {
         "controller": "addSocietyAccount"
     });
 
-    $routeProvider.when("/addMember", {
-        "templateUrl": "./views/master/addMember.html",
-        "controller": "addMember"
-    });
-
 
     $routeProvider.when("/masterAndTransactionPage", {
         "templateUrl": "./views/masterAndTransactionPage.html",
         "controller": "masterAndTransactionPage"
     });
 
+    
+// ............MASTER FOLDER.............
+
+    $routeProvider.when("/addMember", {
+        "templateUrl": "./views/master/addMember.html",
+        "controller": "addMember"
+    });
+
+    $routeProvider.when("/updateMember", {
+        "templateUrl": "./views/master/updateMember.html",
+        "controller": "updateMember"
+    });
+
+// ............TRANSACTION FOLDER.............
+
     $routeProvider.when("/memberReceipt", {
         "templateUrl": "./views/transaction/memberReceipt.html",
         "controller": "memberReceipt"
     });
 
+    $routeProvider.when("/socVoucher", {
+        "templateUrl": "./views/transaction/socVoucher.html",
+        "controller": "socVoucher"
+    });
+    
 });
