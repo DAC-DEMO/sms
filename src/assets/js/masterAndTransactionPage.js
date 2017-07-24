28456413e82331aac2896d1dcaa60dfdf1fc7c68
laxman3194 <laxman3194@gmail.com>
2017-07-24 20:23:35 +0530

    changes done

--- src/assets/js/masterAndTransactionPage.js
+++ src/assets/js/masterAndTransactionPage.js
@@ -5,23 +5,13 @@ app.controller("masterAndTransactionPage", function($scope,$location) {
     // SOURCE OF DATA FOR PAGE1
     $scope.masterAndTransactionPage = { "title" : "Master and Transaction Page" };
 
-    $scope.tabList = [{
+    $scope.tabList = {
         "value" : "addMem"
-    },{
-        "value" : "addSocAcc"
-    },{
-        "value" : "updateMem"
-    },{
-        "value" : "maintananceBill"
-    },{
-        "value" : "memReceipt"
-    },{
-        "value" : "socVoucher"
-    }
-    ];
+    };
+    
 
     $scope.data = {
-        "defaultTab" : $scope.tabList[0].value
+        "defaultTab" : $scope.tabList.value
     };
 
     $scope.changeTab = function(value){
