edbd49cb9669cf4812d84ad598a105917f167905
Kishori <kkpatil19@gmail.com>
2017-07-27 21:22:04 +0530

    changes done in member list js file

---
+++ src/assets/js/master/memberList.js
@@ -0,0 +1,17 @@
+var app = angular.module("app");
+var module1 = require('./backend/module1');
+
+app.controller("memberList", function ($scope,$location) {
+
+    // SOURCE OF DATA FOR PAGE1
+   
+
+    $scope.memberList = [];
+
+    $scope.getMemberList = function(){
+        module1.getMemberList1(function(response){
+            console.log(response);
+        });
+    }
+
+});
